ed8007f47d9a61b50210827293f04a39ac715cdb
Alexey Pavlov <pavlov.aleksey.m@gmail.com>
2020-04-26 18:08:36 +0300

    add babel & fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import program from 'commander';
+import gendiff from 'commander';
 
-program
+gendiff
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'output format');
 
-export default program;
+export default gendiff;
